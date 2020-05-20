bf9dcb63c2eaad1e47a5cefd79e5646c76bf493e
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-20 15:51:37 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -11,7 +11,9 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   collection.length.times do |index|
     var = check_shelf(collection[index], name)
-    puts [var].to_h
+    if not var.nil?
+      return [var].to_h
+    end 
   end
 end
 
