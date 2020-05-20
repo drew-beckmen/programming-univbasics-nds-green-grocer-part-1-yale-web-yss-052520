753fc259f86a0e1326fb44a1831f5c3a767e348a
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-20 15:29:37 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,6 +2,13 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  collection.length.times do |index|
+    collection[index].find do |key, value|
+      if key == name
+        return collection[index]
+      end
+    end
+  end
 
 end
 
@@ -12,6 +19,3 @@ def consolidate_cart(cart)
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
 end
-
-
-  
