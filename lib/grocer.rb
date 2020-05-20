d835e1f9ee9371bd84113e38b072d68fb80db4c7
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-20 15:55:56 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,5 +1,8 @@
 require 'pry'
 
+#Define a helper method that takes in a hash and returns
+# the entire hash AS AN ARRAY if parameter name matches a value in the hash.
+# Returns nil otherwise.
 def check_shelf(shelf_hash, name)
   shelf_hash.find {|key, value| value==name}
 end
@@ -11,6 +14,7 @@ def find_item_by_name_in_collection(name, collection)
   collection.length.times do |index|
     var = check_shelf(collection[index], name)
     if not var.nil?
+      #need to cast back to a hash since find returns an array. 
       return [var].to_h
     end
   end
@@ -22,5 +26,5 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-  
+
 end
