e4753ff2dd7e5187ef09878abcedfc6b45a109c8
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-20 15:41:45 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,11 +1,15 @@
 require 'pry'
+
+def check_shelf(shelf_hash, name)
+  arr = shelf_hash.find {|key, value| value==name}
+  arr.to_h
+end
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
   collection.length.times do |index|
     collection[index].find do |key, value|
-      binding.pry
       value == name
     end
   end
